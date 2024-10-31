edf922c4fa3cbbd007c2f24a326027e882e5205d
Sitri Kamishirasawa <sitri.ndxbn@gmail.com>
2024-10-31 12:06:01 +0900

    feat: fix npm script usage

--- scripts/dev.ts
+++ scripts/dev.ts
@@ -2,4 +2,5 @@ import { $ } from "bun";
 
 if (process.env.NODE_ENV === "development") {
 	await Promise.all([$`bunx lefthook install`]);
+	console.log("To get started developing: edit src/main.ts and `bun start`");
 }
